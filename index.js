11df0647f53ed8aba038ccaabb644bada0d13595
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2024-03-04 19:46:18 +0600

    update

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-function loadPost(){
-    fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
-    .then(res => res.json())
-    .then(data => displayPost(data.posts))
+function loadPost() {
+  fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+    .then((res) => res.json())
+    .then((data) => displayPost(data.posts));
 }
-function displayPost(posts){
-    const postContainer = document.getElementById('post-container');
-    for(const post of posts){
-        const postDiv = document.createElement('div');
-        //console.log(post)
-        postDiv.classList = `card bg-gray-100 m-4 p-4`;
-        postDiv.innerHTML = `
-        <div class="flex" onclick="addToTitleArea()">
-            <div>
+
+function displayPost(posts) {
+  const postContainer = document.getElementById("post-container");
+  postContainer.textContent = "";
+  for (const post of posts) {
+    const postDiv = document.createElement("div");
+    //console.log(post)
+    postDiv.classList = `card bg-gray-100 m-4 p-4`;
+    postDiv.innerHTML = `
+        <div class="flex">
+            <div class="relative">
                 <img class="w-[72px] h-[72] rounded-lg mr-4" src=${post.image}/>
+                <div class="w-4 h-4 rounded-full absolute -top-2 right-2 z-30 ${
+                  post.isActive ? "bg-green-500" : "bg-red-500"
+                }"></div>
             </div>
-            <div>
-                <p>#${post.category}<span class="ml-4">Author: ${post.author.name}</span></p>
-                <h2 class="${post.category} font-bold text-xl my-2">${post.title}</h2>
+            <div class="w-full">
+                <p>#${post.category}<span class="ml-4">Author: ${
+      post.author.name
+    }</span></p>
+                <h2 class="${post.category} font-bold text-xl my-2">${
+      post.title
+    }</h2>
                 <p>${post.description}</p>
-                <div>
-                <i class="fa-regular fa-message mx-2"></i> ${post.comment_count} <i class="fa-regular fa-eye mx-2"></i> ${post.view_count} <i class="fa-regular fa-clock mx-2"></i> ${post.posted_time}
+                <div class="flex justify-between items-center w-full">
+                    <div>
+                        <i class="fa-regular fa-message mx-2"></i> ${
+                          post.comment_count
+                        }
+                        <i class="fa-regular fa-eye mx-2"></i> ${
+                          post.view_count
+                        }
+                        <i class="fa-regular fa-clock mx-2"></i> ${
+                          post.posted_time
+                        }
+                    </div>
+                    <div onclick="addToTitleArea(${post})" class="cursor-pointer hover:bg-green-600 transition-colors rounded-full bg-green-500 w-8 h-8 flex justify-center items-center">
+                        <i class="fa-regular fa-envelope-open text-white"></i>
+                    </div>
                 </div>
             </div>
         </div>
         `;
-        postContainer.appendChild(postDiv);
-    }
+    postContainer.appendChild(postDiv);
+  }
 }
 loadPost();
 
 //eventhandeler
-function addToTitleArea(){
-    //parent
-    const calculationEntry = document.getElementById('title-card');
+let totalClickedCount = 0;
+function addToTitleArea(post) {
+  totalClickedCount++;
+  const count = document.getElementById("clicked-count");
+  count.innerHTML = totalClickedCount;
+  //parent
+  const calculationEntry = document.getElementById("title-card");
 
-    //create child
-    const p = document.createElement('p');
-    p.classList.add('mt-2');
-    p.innerHTML = `Clicked`;
-    //append child with parent
-    calculationEntry.appendChild(p);
+  //create child
+  const p = document.createElement("p");
+  p.classList.add("mt-2");
+  p.innerHTML = `Clicked`;
+  //append child with parent
+  calculationEntry.appendChild(p);
 }
